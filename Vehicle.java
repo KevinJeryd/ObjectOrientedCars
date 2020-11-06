883d3497414b0cbfa5a2529864bb20fa63334959
gabbe <hhPzAGZmBmGQV9H>
2020-11-06 14:51:35 +0100

    Made the interface Movable with the methods move, turnLeft and turnRight.
    removed public access modifier from Vehicle.java (was redundant)

--- Vehicle.java
+++ Vehicle.java
@@ -1,17 +1,17 @@
 import java.awt.*;
 
 public interface Vehicle {
-    public int getNrDoors();
-    public double getEnginePower();
-    public double getCurrentSpeed();
-    public Color getColor();
-    public void setColor(Color clr);
-    public void startEngine();
-    public void stopEngine();
-    public void setCurrentSpeed(double currentSpeed);
-    public double speedFactor();
-    public void incrementSpeed(double amount);
-    public void decrementSpeed(double amount);
-    public void gas(double amount);
-    public void brake(double amount);
+    int getNrDoors();
+    double getEnginePower();
+    double getCurrentSpeed();
+    Color getColor();
+    void setColor(Color clr);
+    void startEngine();
+    void stopEngine();
+    void setCurrentSpeed(double currentSpeed);
+    double speedFactor();
+    void incrementSpeed(double amount);
+    void decrementSpeed(double amount);
+    void gas(double amount);
+    void brake(double amount);
 }
