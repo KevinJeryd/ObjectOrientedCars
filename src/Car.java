c44dbca93b543cd1fd847747c67443e8d69be41c
gabbe <hhPzAGZmBmGQV9H>
2020-11-10 14:44:39 +0100

    Changed the variables nrDoors, enginePower and modelName to final

--- src/Car.java
+++ src/Car.java
@@ -26,11 +26,11 @@ public abstract class Car implements Vehicle, Movable {
      * String modelName; The car model name
      * Direction facing; Facing direction of the car
      */
-    private int nrDoors; // Number of doors on the car
-    private double enginePower; // Engine power of the car
+    private final int nrDoors; // Number of doors on the car
+    private final double enginePower; // Engine power of the car
     private double currentSpeed; // The current speed of the car
     private Color color; // Color of the car
-    private String modelName; // The car model name
+    private final String modelName; // The car model name
 
     private Direction facing;
 
