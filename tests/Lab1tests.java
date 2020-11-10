4bf62d23cdca3ce72986da49ebdda886dd91a295
gabbe <hhPzAGZmBmGQV9H>
2020-11-10 14:44:14 +0100

    Added some test

--- tests/Lab1tests.java
+++ tests/Lab1tests.java
@@ -1,3 +1,39 @@
+import org.junit.Test;
+import static  org.junit.Assert.assertEquals;
+
+/**
+ * Current speed after stopping engine should be 0
+ * Speed should be higher after incrementSpeed
+ * Speed should be lower after decrementSpeed
+ *
+ *
+ */
+
 public class Lab1tests {
+
+    @Test
+    public void stopMotorSpeedEqualZero() {
+        Car car = new Saab95();
+        car.stopEngine();
+        assertEquals(0, car.getCurrentSpeed(), 0);
+    }
+
+    @Test
+    public void higherSpeedAfterIncrementSpeed() {
+        Car car = new Saab95();
+        double speedBeforeInc = car.getCurrentSpeed();
+        car.incrementSpeed(3);
+        double speedAfterInc = car.getCurrentSpeed();
+        assert (speedAfterInc > speedBeforeInc);
+    }
+
+    @Test
+    public void lowerSpeedAfterDecrementSpeed() {
+        Car car = new Saab95();
+        double speedBeforeInc = car.getCurrentSpeed();
+        car.decrementSpeed(3);
+        double speedAfterInc = car.getCurrentSpeed();
+        assert (speedAfterInc < speedBeforeInc);
+    }
 
 }
