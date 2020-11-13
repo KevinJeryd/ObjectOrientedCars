c810df3fdb2d3f1a7878f00b849f78fa5c684372
gabbe <hhPzAGZmBmGQV9H>
2020-11-13 14:25:02 +0100

    Added conditions to setPlatform and move

--- src/Scania.java
+++ src/Scania.java
@@ -18,11 +18,35 @@ public class Scania extends Truck {
         setPlatform(0);
     }
 
+    /**
+     * Sets platform angle (only between 0 and 70) and not while in motion.
+     *
+     * @param platform
+     */
     public void setPlatform(double platform) {
-        this.platform = platform;
+        if (getCurrentSpeed() > 0) {
+            throw new IllegalArgumentException("You can't drive and change the platform!");
+        } else {
+            if (getPlatform() < 0) {
+                this.platform = 0;
+            } else if (getPlatform() > 70) {
+                this.platform = 70;
+            } else {
+                this.platform = platform;
+            }
+        }
     }
 
-    public double getPlatform(double platform) {
+    public double getPlatform() {
         return platform;
+    }
+
+    @Override
+    public void move(){
+        if (getPlatform() == 0) {
+            super.move();
+        } else {
+            throw new IllegalArgumentException("Lower the platform first!");
+        }
     }
 }
