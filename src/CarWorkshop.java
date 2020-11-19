5a3c826dde31a427b2c12d891f2d6d53f3668454
gabbe <hhPzAGZmBmGQV9H>
2020-11-19 13:35:52 +0100

    Added load and unload methods to VehicleWorkshop.java

--- src/CarWorkshop.java
+++ src/CarWorkshop.java
@@ -1,2 +1,3 @@
-public class CarWorkshop {
+public class CarWorkshop extends VehicleWorkshop<Car> {
+
 }
--- src/VehicleWorkshop.java
+++ src/VehicleWorkshop.java
@@ -1,10 +1,32 @@
-import java.awt.*;
 import java.util.*;
 
-public abstract class VehicleWorkshop<T> {
-    private ArrayList<T> workshopCapacity;
+public abstract class VehicleWorkshop<T extends Vehicle> {
+    private final int maxCapacity;
+    private final ArrayList<T> vehicles = new ArrayList<>();
 
-    public VehicleWorkshop() {
-
+    public VehicleWorkshop(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
     }
+
+    public ArrayList<T> getVehicles() {
+        return vehicles;
+    }
+
+    public void load(T vehicle) {
+        if (getVehicles().size() <= maxCapacity) {
+            getVehicles().add(vehicle);
+        } else {
+            throw new IndexOutOfBoundsException("The workshop is full.");
+        }
+    }
+
+    public T unload(int i) {
+        if (!getVehicles().isEmpty()) {
+            return getVehicles().remove(i);
+        } else {
+            throw new IndexOutOfBoundsException("The workshop is empty.");
+        }
+    }
+
+
 }
